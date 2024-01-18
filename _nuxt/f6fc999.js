b12d50fedc73aa2b909513b7265d3237a47139e8
Jose Q <jose.quitain@readytech.io>
2024-01-18 14:20:06 +1100

    test 2

---
+++ _nuxt/f6fc999.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{427:function(t,n,o){"use strict";o.r(n);var c={name:"sortIcon",computed:{},methods:{}},r=o(58),component=Object(r.a)(c,(function(){var t=this._self._c;return t("span",{staticClass:"float-right"},[t("b-icon",{attrs:{icon:"arrow-down-up"}})],1)}),[],!1,null,"05a07955",null);n.default=component.exports}}]);
